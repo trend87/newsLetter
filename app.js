c1c55592dd9ae4498a6f66f29311971a23136206
Effiong Israel Robert <israelrobert@gmail.com>
2022-10-26 22:07:43 +0100

    created local server to listen at port 3000 and sent a browser respond

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const request = require("request")
 const bodyParser= require("body-parser")
 
 const app = express()
+
+
+app.get("/", function(req, res){
+    res.send("Hello")
+})
+
+app.listen(3000, function(req, res){
+    console.log("Server Up and Running at port 3000")
+})
