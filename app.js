9b09602a8edce3b18ce12d97ad235c9cd37740b6
Effiong Israel Robert <israelrobert@gmail.com>
2022-10-29 22:11:51 +0100

    grabbed data using app.post function and created .gitignore

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ const bodyParser= require("body-parser")
 const app = express()
 
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
 
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/", function(req, res){
+    var firstName = req.body.fName
+    var lastName = req.body.lName
+    var email = req.body.email
+
+    console.log(firstName, lastName, email)
 })
 
 app.listen(3000, function(req, res){
